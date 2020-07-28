b40e34ab81c7e63f5fc8c791d51889f73426ea45
leecw543 <leecw543@gmail.com>
2020-07-28 20:02:35 +0800

    加入專案檔案。

---
+++ prjVidlyDemo/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using prjVidlyDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjVidlyDemo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+      var movie = new Movie() { Name = "屍速列車：感染半島" };
+            return View(movie);
+        }
+    }
+}
